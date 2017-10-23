cc237bc679c50938f37af407f4cef76474734632
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2017-10-23 12:20:00 +0200

    Test metadata

---
+++ NewPicasa/view/winTestMetadata.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace NewPicasa.view
+{
+    /// <summary>
+    /// Logique d'interaction pour winTestMetadata.xaml
+    /// </summary>
+    public partial class winTestMetadata : Window
+    {
+        public winTestMetadata()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetMetadata_Click(object sender, RoutedEventArgs e)
+        {
+            string strFile = @"C:\Users\Benjamin.Delacombaz\Desktop\Test\Test.jpg";
+            FileStream fleFile = File.Open(strFile, FileMode.Open);
+            Bitmap btmFile = new Bitmap(fleFile);
+            fleFile.Dispose();
+        }
+    }
+}
