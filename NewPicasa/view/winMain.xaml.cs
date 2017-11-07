bbc4193cfb93fc54e2951b331feac0e286f5eaaa
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2017-11-07 16:06:59 +0100

    Add TreeView Event on click (In Progress)

--- NewPicasa/view/winMain.xaml.cs
+++ NewPicasa/view/winMain.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,5 +24,43 @@ namespace NewPicasa.view
         {
             InitializeComponent();
         }
+
+        // EVENTS
+        // ----------------------------------------------------------------------------
+        private void trvMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            f_ListDirectory(trvMain, @"C:\Users\Benjamin.Delacombaz\Desktop\lst_photo");
+        }
+        // ----------------------------------------------------------------------------
+
+        // FUNCTIONS
+        // ----------------------------------------------------------------------------
+        private void f_ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Items.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Items.Add(f_CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        private static TreeViewItem f_CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeViewItem { Header = directoryInfo.Name };
+            directoryNode.MouseLeftButtonUp += DirectoryNode_MouseLeftButtonUp;
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                directoryNode.Items.Add(f_CreateDirectoryNode(directory));
+            }
+            return directoryNode;
+
+        }
+
+        private static void DirectoryNode_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            TreeViewItem item = sender as TreeViewItem;
+            MessageBox.Show(item.Header.ToString());
+        }
+        // ----------------------------------------------------------------------------
+
     }
+
 }
